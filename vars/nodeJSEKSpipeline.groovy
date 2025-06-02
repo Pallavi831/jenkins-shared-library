e6e26aed7a9256ae4942af439f9b8e5792e8f2af
Pallavi C <info@joindevops.com>
2025-06-02 22:21:06 +0530

    jenkins

--- vars/nodeJSEKSpipeline.groovy
+++ vars/nodeJSEKSpipeline.groovy
@@ -10,23 +10,80 @@ def call(Map configmap) {
         }
 
         environment {
-            greeting = configMap.get('greeting')
+            DEBUG = 'true'
+            appVersion = '' // Global variable, set in first stage
+            region = 'us-east-1'
+            account_id = '557690626059'
+            project = configMap.get('project')
+            component = configMap.get('component')
+            environment = 'dev'
         }
         parameters{
             booleanParam(name: 'deploy', defaultValue: false, description: 'Toggle this value')
         }
         stages {
 
-            stage('print greeting') {
+            stage('Read the version') {
                 steps {
                     script {
-                       
-                        echo "Version is : $greeting"
+                        def packageJson = readJSON file: 'package.json'
+                        appVersion = packageJson.version
+                        echo "App version: ${appVersion}"
                     }
                 }
             }
 
-            
+            stage('Install Dependencies') {
+                steps {
+                    sh 'npm install'
+                }
+            }
+            // stage('Run Sonarqube') {
+            //     environment {
+            //         scannerHome = tool 'sonar-scanner-7.1';
+            //     }
+            //     steps {
+            //     withSonarQubeEnv('sonar-scanner-7.1') {
+            //         sh "${scannerHome}/bin/sonar-scanner"
+            //         // this is generic command works for any language
+            //     }
+            //     }
+            // }
+            // stage("Quality Gate") {
+            //     steps {
+            //     timeout(time: 1, unit: 'HOURS') {
+            //         waitForQualityGate abortPipeline: true
+            //     }
+            //     }
+            // } 
+            stage('Docker build') {
+                steps {
+                    withAWS(region: "${region}", credentials: 'aws-creds') {
+                        sh """
+                            aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-east-1.amazonaws.com
+
+                            docker build -t ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${environment}/${component}:${appVersion} .
+
+                        
+
+                            docker push ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${environment}/${component}:${appVersion}
+                        """
+                    }
+                }
+            }
+
+        
+
+            stage('Trigger Deploy'){
+                when { 
+                    expression { params.deploy }
+                }
+                steps{
+                    build job: 'backend-cd', parameters: [string(name: 'version', value: "${appVersion}")], wait: true
+                }
+            }
+        }
+
         post {
             always {
                 echo "This section runs always"
